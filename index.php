f1a863e381d1a40940e56613620f6b07899b2e76
Boxin Wang <me@co8bit.com>
2014-02-02 10:29:53 +0800

    创建项目

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+define('APP_NAME','client');
+define('APP_PATH','./client/');
+
+define('APP_DEBUG',TRUE); 
+
+define('ENGINE_NAME','SAE');
+require '../ThinkPHP/ThinkPHP.php';
---
+++ server.php
@@ -0,0 +1,8 @@
+<?php
+define('APP_NAME','server');
+define('APP_PATH','./server/');
+
+define('APP_DEBUG',TRUE); 
+
+define('ENGINE_NAME','SAE');
+require '../ThinkPHP/ThinkPHP.php';
